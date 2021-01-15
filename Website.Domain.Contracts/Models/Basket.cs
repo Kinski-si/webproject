1003b20e994e4d9ab97905437f7eef022fee08e3
artem0513 <57153181+artem0513@users.noreply.github.com>
2021-01-15 21:06:10 +0200

    update ignore

--- Website.Domain.Contracts/Models/Basket.cs
+++ Website.Domain.Contracts/Models/Basket.cs
@@ -5,7 +5,7 @@ namespace Website.Domain.Contracts.Models
     public class Basket : ShopModelBase
     {
         public float TotalPrice { get; set; }
-
+        
         public IEnumerable<Clothes> Clothes { get; set; }
     }
 }   
